c45a2a518202b9637f32396559203de90fa69b61
maxkatilius <max.katilius@gmail.com>
2025-08-14 16:33:38 +1000

    description for each project title added

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -30,18 +30,21 @@ const Projects = () => {
                 </div>
                 <div className="flex items-start justify-center gap-10">
                     <div className="flex flex-col gap-4 xl:gap--8 xl:w-1/2 md:text-[1.2rem] lg:text-[1.2rem] xl:text-[1.35rem]">
-                        <h3 className="text-center font-[500] text-[1.5rem] md:text-[1.8rem] lg:text-[1.9rem] xl:text-[2rem] tracking-wide py-1">
-                            <a 
-                                href="https://crocodile-mk.netlify.app"
-                                rel="noreferrer"
-                                target="_blank"
-                                className="relative group inline-block font-[Quicksand] tracking-widest hover:text-[#0B6E4F] transition-all duration-300"
-                            >
-                                CROCODILE
-                                <span className="absolute -bottom-1 left-0 h-1 w-0 bg-[#0B6E4F] transition-all duration-400 ease-in-out group-hover:w-full"></span>
-                                <span className="absolute -top-1 left-0 h-1 w-0 bg-[#0B6E4F] transition-all duration-400 ease-in-out group-hover:w-full"></span>
-                            </a>
-                        </h3>
+                        <div className="flex gap-3 items-center justify-center">
+                            <h3 className="text-center font-[500] text-[1.5rem] md:text-[1.8rem] lg:text-[1.9rem] xl:text-[2rem] tracking-wide py-1">
+                                <a 
+                                    href="https://crocodile-mk.netlify.app"
+                                    rel="noreferrer"
+                                    target="_blank"
+                                    className="relative group inline-block font-[Quicksand] tracking-widest hover:text-[#0B6E4F] transition-all duration-300"
+                                    >
+                                    CROCODILE
+                                    <span className="absolute -bottom-1 left-0 h-1 w-0 bg-[#0B6E4F] transition-all duration-400 ease-in-out group-hover:w-full"></span>
+                                    <span className="absolute -top-1 left-0 h-1 w-0 bg-[#0B6E4F] transition-all duration-400 ease-in-out group-hover:w-full"></span>
+                                </a>
+                            </h3>
+                            <p>- Mock Ecommerce Store</p>
+                        </div>
                         <div className="group overflow-hidden rounded border-4 xl:hidden">
                             <a
                                 href="https://crocodile-mk.netlify.app"
@@ -186,19 +189,22 @@ const Projects = () => {
                 <hr className="hidden xl:block w-full border-4 border-orange-500 -mb-4" />
                 <div className="flex items-start justify-center gap-10">
                     <div className="flex flex-col gap-4 xl:gap-8 xl:w-1/2 md:text-[1.2rem] lg:text-[1.2rem] xl:text-[1.35rem]">
-                        <h3 className="text-center font-bold text-[1.5rem] md:text-[1.8rem] lg:text-[1.9rem] xl:text-[2rem]">
-                        <a 
-                            href="https://palatable.netlify.app/"
-                            rel="noreferrer"
-                            target="_blank"
-                            className="relative group inline-block font-['M_PLUS_Rounded_1c'] font-[400] tracking-widest transition-colors duration-300"
-                        >
-                            <span className="relative z-10 bg-[linear-gradient(to_right,_#257DD0,_#ED5FC2,_#EFAD72,_#22C3A3)] bg-clip-text hover:text-transparent transition-all duration-400 ease-in-out">
-                                palatable
-                            </span>
-                            <span className="absolute -bottom-1 left-0 h-1 w-0 bg-[linear-gradient(to_right,_#257DD0,_#ED5FC2,_#EFAD72,_#22C3A3)] transition-all duration-400 ease-in-out group-hover:w-full"></span>
-                        </a>
-                        </h3>
+                        <div className="flex gap-3 items-center justify-center">
+                            <h3 className="text-center font-bold text-[1.5rem] md:text-[1.8rem] lg:text-[1.9rem] xl:text-[2rem]">
+                                <a 
+                                    href="https://palatable.netlify.app/"
+                                    rel="noreferrer"
+                                    target="_blank"
+                                    className="relative group inline-block font-['M_PLUS_Rounded_1c'] font-[400] tracking-widest transition-colors duration-300"
+                                    >
+                                    <span className="relative z-10 bg-[linear-gradient(to_right,_#257DD0,_#ED5FC2,_#EFAD72,_#22C3A3)] bg-clip-text hover:text-transparent transition-all duration-400 ease-in-out">
+                                        palatable
+                                    </span>
+                                    <span className="absolute -bottom-1 left-0 h-1 w-0 bg-[linear-gradient(to_right,_#257DD0,_#ED5FC2,_#EFAD72,_#22C3A3)] transition-all duration-400 ease-in-out group-hover:w-full"></span>
+                                </a>
+                            </h3>
+                            <p>- Interactive Colour Palette Generator</p>
+                        </div>
                         <div className="group overflow-hidden rounded border-4 xl:hidden">
                             <a
                                 href="https://palatable.netlify.app/"
@@ -311,19 +317,23 @@ const Projects = () => {
                 <hr className="hidden xl:block w-full border-4 border-orange-500 -mb-4" />
                 <div className="flex items-start justify-center gap-10 mb-8 2xl:mb-12">
                     <div className="flex flex-col gap-4 xl:w-1/2 xl:gap-8 md:text-[1.2rem] lg:text-[1.2rem] xl:text-[1.35rem]">
-                        <h3 className="text-center font-bold text-[1.5rem] md:text-[1.8rem] lg:text-[1.9rem] xl:text-[2rem] py-1">
-                        <a 
-                                href="https://clockwise-mk.netlify.app/"
-                                rel="noreferrer"
-                                target="_blank"
-                                className="relative group inline-block font-[Lato] font-[300] tracking-wide transition-all duration-500"
-                            >
-                                <span className="transition-all duration-500 group-hover:bg-gradient-to-r group-hover:from-sky-600 group-hover:to-blue-500 group-hover:bg-clip-text group-hover:text-transparent">
-                                    CLOCK<span className="font-bold">WISE</span>
-                                </span>
-                                <span className="absolute -bottom-1 left-0 h-1 w-0 bg-linear-to-r from-sky-200 to-blue-400 transition-all duration-600 ease-in-out group-hover:w-full"></span>
-                            </a>
-                        </h3>
+                        <div className="flex gap-3 items-center justify-center">
+
+                            <h3 className="text-center font-bold text-[1.5rem] md:text-[1.8rem] lg:text-[1.9rem] xl:text-[2rem] py-1">
+                                <a 
+                                    href="https://clockwise-mk.netlify.app/"
+                                    rel="noreferrer"
+                                    target="_blank"
+                                    className="relative group inline-block font-[Lato] font-[300] tracking-wide transition-all duration-500"
+                                    >
+                                    <span className="transition-all duration-500 group-hover:bg-gradient-to-r group-hover:from-sky-600 group-hover:to-blue-500 group-hover:bg-clip-text group-hover:text-transparent">
+                                        CLOCK<span className="font-bold">WISE</span>
+                                    </span>
+                                    <span className="absolute -bottom-1 left-0 h-1 w-0 bg-linear-to-r from-sky-200 to-blue-400 transition-all duration-600 ease-in-out group-hover:w-full"></span>
+                                </a>
+                            </h3>
+                            <p>- Instantaneous Timezone Converter</p>
+                        </div>
                         <div className="group overflow-hidden rounded border-4 xl:hidden">
                             <a
                                 href="https://clockwise-mk.netlify.app/"
