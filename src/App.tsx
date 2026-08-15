a13551bc8ca8ef0fcf6f666b05bbc56654efa402
Max Katilius <max.katilius@gmail.com>
2026-08-15 12:25:04 +1000

    Updated CV

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,16 @@
-import Header from './components/Header'
-import Footer from './components/Footer'
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 
-import Home from './pages/Home'
+import Home from "./pages/Home";
 
 function App() {
-
-  return (
-    <div className='min-h-dvh min-w-dvw flex flex-col bg-white font-[Fira_Mono]'>
-      <Header />
-      <Home />
-      <Footer />
-    </div>
-  )
+	return (
+		<div className="min-h-dvh min-w-dvw flex flex-col bg-white font-[Fira_Mono]">
+			<Header />
+			<Home />
+			<Footer />
+		</div>
+	);
 }
 
-export default App
+export default App;
